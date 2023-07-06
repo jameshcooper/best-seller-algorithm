4defa1913c16231c47ea1fcbbec10de8733a7e48
James Cooper <jameshcooper13@gmail.com>
2023-07-07 09:13:55 +1000

    trims

--- 2.sql
+++ 2.sql
@@ -3,16 +3,16 @@ use CrowdSourced;
 -- each customers top 3 items   
 select
     c1.Customer_ID,
-    c1.UPC 'SKU_1',
-    c2.UPC 'SKU_2',
-    c3.UPC 'SKU_3' into CrowdSourced.dbo.faves_top3
+    c1.SKU 'SKU_1',
+    c2.SKU 'SKU_2',
+    c3.SKU 'SKU_3' into faves_top3
 from
-    CrowdSourced.dbo.cust_clothing_all c1
-    inner join CrowdSourced.dbo.cust_clothing_all c2 on (
+    cust_clothing_all c1
+    inner join cust_clothing_all c2 on (
         c2.Customer_ID = c1.Customer_ID
         and c2.rank = 2
     )
-    inner join CrowdSourced.dbo.cust_clothing_all c3 on (
+    inner join cust_clothing_all c3 on (
         c3.Customer_ID = c1.Customer_ID
         and c3.rank = 3
     )
